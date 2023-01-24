4c74b6bb860833bb2420d10fbb535df43d800486
Taylor McPherson <taylor@lifeblue.com>
2023-01-24 12:43:32 -0800

    bring back cli

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,6 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from 'sanity'
+import { deskTool } from 'sanity/desk'
+import { schemaTypes } from './schemas'
 import { markdownSchema } from 'sanity-plugin-markdown'
 import { media } from 'sanity-plugin-media'
 
@@ -11,7 +10,7 @@ export default defineConfig({
   projectId: '38t6rk2v',
   dataset: 'production',
 
-  plugins: [deskTool(), visionTool(), markdownSchema(), media()],
+  plugins: [deskTool(), markdownSchema(), media()],
 
   schema: {
     types: schemaTypes,
