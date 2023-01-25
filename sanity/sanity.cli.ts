a6b8e526b2bd095ea0f19c460e9b9f438c55d978
Taylor McPherson <taylor@lifeblue.com>
2023-01-24 19:11:07 -0800

    remove sanity, move into its own repo

--- sanity/sanity.cli.ts
+++
@@ -1,8 +0,0 @@
-import { defineCliConfig } from 'sanity/cli';
-
-export default defineCliConfig({
-  api: {
-    projectId: '38t6rk2v',
-    dataset: 'production'
-  }
-});
--- sanity/schemas/index.ts
+++
@@ -1,5 +0,0 @@
-
-import page from './page'
-import project from './project'
-
-export const schemaTypes = [page, project]
